51717db7daf174767ebe3e78ad8be16b915df729
Kenneth Tan <kennethtan1414@gmail.com>
2023-07-20 22:24:36 +0800

    changes

--- utils/checkDate.js
+++ utils/checkDate.js
@@ -52,8 +52,29 @@ const chatDate = (createAt) => {
     }
 }
 
+const lastChat = (createAt) =>{
+    const time = new Date(createAt);
+    const now = new Date();
+    const diff = (now.getTime() - time.getTime()) / 1000;
+
+    if (diff < 60) {
+        return 'now';
+    } else if (diff < 3600) {
+        return Math.round(diff / 60) + 'm';
+    } else if (diff < 86400) {
+        return Math.round(diff / 3600) + 'h';
+    } else if (diff < 604800) {
+        return Math.round(diff / 86400) + 'd';
+    } else if (diff < 31536000) {
+        return Math.round(diff / 604800) + 'w';
+    } else if (diff >= 31536000) {
+        return Math.round(diff / 31536000) + 'y';
+    }
+}
+
 module.exports = {
     checkDate,
     checkMDY,
-    chatDate
+    chatDate,
+    lastChat
 }
